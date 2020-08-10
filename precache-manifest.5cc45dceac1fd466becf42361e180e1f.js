279a145dea83d229316f574f60feb6b44098cc26
aakash <aakashgarg100@gmail.com>
2020-08-11 01:56:07 +0530

    Updates

---
+++ precache-manifest.5cc45dceac1fd466becf42361e180e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf4cbc6a766f5be8c7a2a9c9f5141fe",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "171ad365d570d70d0bea",
+    "url": "/To-Do-App/static/css/main.4f360400.chunk.css"
+  },
+  {
+    "revision": "fddffd3fb722a490a6e2",
+    "url": "/To-Do-App/static/js/2.f94ae0e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-App/static/js/2.f94ae0e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "171ad365d570d70d0bea",
+    "url": "/To-Do-App/static/js/main.1c57bc1e.chunk.js"
+  },
+  {
+    "revision": "bb086c03b1181a5a4cf0",
+    "url": "/To-Do-App/static/js/runtime-main.69698141.js"
+  }
+]);
